f732a56b0a056db40b8f397715c3c114c0269eff
Leonardo Andre Nunes Meduna <leonardo.meduna231@gmail.com>
2025-07-15 12:31:25 -0300

    matrizes/primeiro exercicio

---
+++ algebra_linear/notebook/exercises.py
@@ -0,0 +1,32 @@
+# %%
+import numpy as np
+import math
+import seaborn as sns
+import matplotlib.pyplot as plt
+# %%
+# Suponhamos que a seguinte matriz forneça as quantidades das vitaminas A, B e C obtidas em cada unidade dos alimentos I e II.
+matriz_vitaminas = np.array([
+    [4, 3, 0],
+    [5, 0, 1]
+])
+print(matriz_vitaminas)
+print(matriz_vitaminas.shape)
+
+# Se ingerirmos 5 unidades do Alimento I e 2 unidades do Alimento II, quanto consumiremos de cada tipo de vitamina?
+
+quantidades_consumidas = np.array([
+    [5, 2]
+])
+total_vitaminas = quantidades_consumidas @ matriz_vitaminas  
+total_vitaminas
+# %%
+# Se o custo dos alimentos depender somente do seu conteúdo vitamínico e soubermos que os preços por unidade de vitamina A, B e C
+# são, respectivamente, 1.5, 3 e 5 u.c.p., quanto pagaríamos pela porção de alimentos indicada anteriormente?
+precos = np.array([
+    [1.5, 3, 5]
+])
+pagamento_p_porcao = (total_vitaminas * precos).sum()
+pagamento_p_porcao
+
+# Ou seja, pagaríamos 100 u.c.p.
+
